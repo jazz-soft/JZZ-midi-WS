57580aa2d9c65f1b9e291290f64f871a50ba6c90
Sema <jazz-dev@jazz-soft.net>
2025-03-17 20:56:52 -0400

    Tests - in progress

--- test/mocha.js
+++ test/mocha.js
@@ -1,13 +1,17 @@
 var assert = require('assert');
 var JZZ = require('jzz');
 
-function WS() {
+function WS(url) {
+  if (!url.startsWith('ws://') && !url.startsWith('wss://')) throw 'bad url';
 }
 global.WebSocket = WS;
 require('..')(JZZ);
 
 describe('WebSocket', function() {
-  it('dummy', function() {
-    JZZ.WS.connect('ws://localhost:8888');
+  it('bad url', function(done) {
+    JZZ.WS.connect('bad url').or(done);
+  });
+  it('no connection', function(done) {
+    JZZ.WS.connect('ws://localhost:8888', 100).then(undefined, function() { done(); });
   });
 });
