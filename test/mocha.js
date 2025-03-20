53c57179653cf7602178f27ddc9a4c1759cc48d0
Sema <jazz-dev@jazz-soft.net>
2025-03-20 19:52:14 -0400

    Reconnect test

--- test/mocha.js
+++ test/mocha.js
@@ -6,7 +6,7 @@ function nop() {}
 function schedule(fn) { setTimeout(fn, 1); }
 
 function WS(url) {  // client
-  if (!url.startsWith('ws://') && !url.startsWith('wss://')) throw 'bad url';
+  if (!url.startsWith('ws://') && !url.startsWith('wss://')) throw { message: 'bad url' };
   var self = this;
   var refl = {
     onerror: nop,
@@ -17,6 +17,7 @@ function WS(url) {  // client
       if (evt == 'close') this.onclose = fn;
       if (evt == 'message') this.onmessage = fn;
     },
+    close: function() { schedule(function() { self.onclose(); }) },
     send: function(data) { self.onmessage({ data: data }); }
   };
   this.send = function(data) { refl.onmessage(data); }
@@ -27,25 +28,38 @@ function WS(url) {  // client
 WS.prototype.onerror = nop;
 WS.prototype.onclose = nop;
 WS.prototype.onmessage = nop;
+WS.prototype.close = function() { this.onclose(); }
 
 function WSS(url) { // server
   this.url = url;
+  this.clients = [];
   this.handle = { connect: nop };
 }
 WSS.prototype.connect = function() { if (!Net[this.url]) Net[this.url] = this; };
 WSS.prototype.onconnection = nop;
 WSS.prototype.on = function(evt, fn) { if (evt == 'connection') this.onconnection = fn; };
-WSS.prototype.client = function(ws) { this.onconnection(ws); };
+WSS.prototype.client = function(ws) {
+  this.clients.push(ws);
+  this.onconnection(ws);
+};
+WSS.prototype.disconnect = function() {
+  if (Net[this.url]) delete Net[this.url];
+  for (var ws of this.clients) ws.close();
+};
 
 global.WebSocket = WS;
 require('..')(JZZ);
+
+function sleep(t) {
+  return new Promise(function(res, rej) { setTimeout(res, t); });
+}
 
 describe('WebSocket', function() {
   it('bad url', function(done) {
     JZZ.WS.connect('bad url').or(done);
   });
   it('no connection', function(done) {
-    JZZ.WS.connect('ws://localhost:8888', 100).then(undefined, function() { done(); });
+    JZZ.WS.connect('ws://localhost:8888', 20).then(undefined, function() { done(); });
   });
   it('connected', function(done) {
     var url = 'ws://connected'; // by the test name
@@ -73,10 +87,35 @@ describe('WebSocket', function() {
     var server_midi_in = new JZZ.Widget();
     server.addMidiIn('midi_in', server_midi_in);
     wss.connect();
-    JZZ.WS.connect(url).and(function() {
+    var client = JZZ.WS.connect(url).and(function() {
       var port = JZZ({ engine: 'none' }).openMidiIn('ws://receive_midi - midi_in');
-      port.connect(function(msg) { done(); });
+      port.connect(function(msg) { client.close(); done(); });
       server_midi_in.noteOn(0, 'C6');
     }).or('Cannot connect');
+  });
+  it('reconnect', async function() {
+    this.timeout(6000);
+    var url = 'ws://reconnect';
+    var wss = new WSS(url);
+    var server = new JZZ.WS.Server(wss);
+    var widget = new JZZ.Widget();
+    wss.connect();
+    server.addMidiIn('w1', widget);
+    server.addMidiOut('w1', widget);
+    server.removeMidiIn('w2');
+    server.removeMidiOut('w2');
+    await JZZ.WS.connect(url);
+    var info = await JZZ({ engine: 'none' }).info();
+    assert.equal(info.inputs[0].name, 'ws://reconnect - w1');
+    wss.disconnect();
+    await sleep(100);
+    server.addMidiIn('w2', widget);
+    server.addMidiOut('w2', widget);
+    server.removeMidiIn('w1');
+    server.removeMidiOut('w1');
+    wss.connect();
+    await sleep(5000);
+    info = await JZZ().info();
+    assert.equal(info.inputs[0].name, 'ws://reconnect - w2');
   });
 });
